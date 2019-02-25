2a131b88f73089a6f20524e3b66e820ab5f10eff
akiyosi <akiyoshi.maruyama@gmail.com>
2019-02-25 17:41:41 +0900

    Add icons

--- qframelesswindow/qframelesswindow.go
+++ qframelesswindow/qframelesswindow.go
@@ -1,6 +1,8 @@
 package qframelesswindow
 
 import (
+	"fmt"
+
         "github.com/therecipe/qt/core"
         "github.com/therecipe/qt/gui"
         "github.com/therecipe/qt/widgets"
@@ -11,7 +13,8 @@ type QFramelessWindow struct {
 	Widget         *widgets.QWidget
 	Layout         *widgets.QGridLayout
 
-	WindowWidget   *widgets.QFrame
+	//WindowWidget   *widgets.QFrame
+	WindowWidget   *widgets.QWidget
 	WindowVLayout  *widgets.QVBoxLayout
 
 	TitleBar       *widgets.QWidget
@@ -19,10 +22,21 @@ type QFramelessWindow struct {
 	TitleLabel     *widgets.QLabel
 	TitleBarBtnWidget *widgets.QWidget
 	TitleBarBtnLayout *widgets.QHBoxLayout
+
 	BtnMinimize    *widgets.QToolButton
 	BtnMaximize    *widgets.QToolButton
 	BtnRestore     *widgets.QToolButton
 	BtnClose       *widgets.QToolButton
+
+	lefttop        *widgets.QSizeGrip
+	top            *widgets.QSizeGrip
+	righttop       *widgets.QSizeGrip
+	left           *widgets.QSizeGrip
+	right          *widgets.QSizeGrip
+	leftbottom     *widgets.QSizeGrip
+	bottom         *widgets.QSizeGrip
+	rightbottom    *widgets.QSizeGrip
+
 
 	Content        *widgets.QWidget
 
@@ -37,6 +51,7 @@ func NewQFramelessWindow() *QFramelessWindow {
 	f.Widget = widgets.NewQWidget(nil, 0)
 	f.Window.SetCentralWidget(f.Widget)
 	f.setupUI(f.Widget)
+	f.SetStyles("#fff")
 	f.setWindowFlags()
 	f.setAttribute()
 
@@ -48,42 +63,17 @@ func (f *QFramelessWindow) setupUI(widget *widgets.QWidget) {
 
 	widget.SetObjectName("QFramelessWindow")
 	widget.SetAttribute(core.Qt__WA_TranslucentBackground, true)
-	widget.SetStyleSheet(" .QFramelessWindow { background-color: rgba(0, 0, 0, 0);}")
 
         f.Layout = widgets.NewQGridLayout(widget)
         f.Layout.SetContentsMargins(0, 0, 0, 0)
 	f.Layout.SetSpacing(0)
 
-	// prepare sizegrip
-	lefttop := widgets.NewQSizeGrip(widget)
-	top := widgets.NewQSizeGrip(widget)
-	righttop := widgets.NewQSizeGrip(widget)
-	left := widgets.NewQSizeGrip(widget)
-	right := widgets.NewQSizeGrip(widget)
-	leftbottom := widgets.NewQSizeGrip(widget)
-	bottom := widgets.NewQSizeGrip(widget)
-	rightbottom := widgets.NewQSizeGrip(widget)
+        // f.WindowWidget = widgets.NewQFrame(widget, 0)
+	// f.WindowWidget.SetStyleSheet(" .QFrame { border: 2px solid #ccc; border-radius: 11px; background-color: #ccc; }")
+        f.WindowWidget = widgets.NewQWidget(widget, 0)
 
-	top.SetSizePolicy2(widgets.QSizePolicy__Expanding, widgets.QSizePolicy__Fixed)
-	left.SetSizePolicy2(widgets.QSizePolicy__Fixed, widgets.QSizePolicy__Expanding)
-	right.SetSizePolicy2(widgets.QSizePolicy__Fixed, widgets.QSizePolicy__Expanding)
-	bottom.SetSizePolicy2(widgets.QSizePolicy__Expanding, widgets.QSizePolicy__Fixed)
-
-	lefttop.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); width: 4px; height: 4px;}")
-	top.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); height: 4px;}")
-	righttop.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); width: 4px; height: 4px;}")
-	left.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); width: 4px; }")
-	right.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); width: 4px; }")
-	leftbottom.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); width: 4px; height: 4px;}")
-	bottom.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); height: 4px;}")
-	rightbottom.SetStyleSheet(" * { background-color: rgba(0, 0, 0, 0); width: 4px; height: 4px;}")
-
-
-        //f.WindowWidget = widgets.NewQWidget(widget, 0)
-        f.WindowWidget = widgets.NewQFrame(widget, 0)
         //f.WindowWidget.SetObjectName("QFramelessWidget")
 	f.WindowWidget.SetSizePolicy2(widgets.QSizePolicy__Expanding | widgets.QSizePolicy__Maximum , widgets.QSizePolicy__Expanding)
-	f.WindowWidget.SetStyleSheet(" .QFrame { border: 2px solid #ccc; border-radius: 11px; background-color: #cccccc; }")
 
 	// windowVLayout is the following structure layout
 	// +-----------+
@@ -112,40 +102,32 @@ func (f *QFramelessWindow) setupUI(widget *widgets.QWidget) {
         f.TitleBarLayout.SetSpacing(0)
 
         f.TitleLabel = widgets.NewQLabel(nil, 0)
-        f.TitleLabel.SetObjectName("titleLabel")
+        f.TitleLabel.SetObjectName("TitleLabel")
         f.TitleLabel.SetAlignment(core.Qt__AlignCenter)
         f.TitleBarLayout.AddWidget(f.TitleLabel, 0, 0)
 
 	btnSizePolicy := widgets.NewQSizePolicy2(widgets.QSizePolicy__Fixed, widgets.QSizePolicy__Fixed, widgets.QSizePolicy__ToolButton)
 
-	// f.TitleBarBtnWidget = widgets.NewQWidget(f.TitleBar, 0)
-	// f.TitleBarBtnWidget.SetSizePolicy2(widgets.QSizePolicy__Fixed, widgets.QSizePolicy__Fixed)
-        // f.TitleBarBtnLayout = widgets.NewQHBoxLayout2(f.TitleBarBtnWidget)
-        // f.TitleBarBtnLayout.SetContentsMargins(0, 0, 0, 0)
-        // f.TitleBarBtnLayout.SetSpacing(0)
-        // f.TitleBarLayout.AddWidget(f.TitleBarBtnWidget, 0, 0)
-
         f.TitleBarLayout.SetAlignment(f.TitleBarBtnWidget, core.Qt__AlignRight)
-        //f.TitleBarBtnLayout.SetAlignment(f.TitleBar, core.Qt__AlignRight)
 
         f.BtnMinimize = widgets.NewQToolButton(f.TitleBar)
-        f.BtnMinimize.SetObjectName("btnMinimize")
+        f.BtnMinimize.SetObjectName("BtnMinimize")
         f.BtnMinimize.SetSizePolicy(btnSizePolicy)
         f.TitleBarLayout.AddWidget(f.BtnMinimize, 0, 0)
 
+        f.BtnMaximize = widgets.NewQToolButton(f.TitleBar)
+        f.BtnMaximize.SetObjectName("BtnMaximize")
+        f.BtnMaximize.SetSizePolicy(btnSizePolicy)
+        f.TitleBarLayout.AddWidget(f.BtnMaximize, 0, 0)
+
         f.BtnRestore = widgets.NewQToolButton(f.TitleBar)
-        f.BtnRestore.SetObjectName("btnRestore")
+        f.BtnRestore.SetObjectName("BtnRestore")
         f.BtnRestore.SetSizePolicy(btnSizePolicy)
         f.BtnRestore.SetVisible(false)
         f.TitleBarLayout.AddWidget(f.BtnRestore, 0, 0)
 
-        f.BtnMaximize = widgets.NewQToolButton(f.TitleBar)
-        f.BtnMaximize.SetObjectName("btnMaximize")
-        f.BtnMaximize.SetSizePolicy(btnSizePolicy)
-        f.TitleBarLayout.AddWidget(f.BtnMaximize, 0, 0)
-
         f.BtnClose = widgets.NewQToolButton(f.TitleBar)
-        f.BtnClose.SetObjectName("btnClose")
+        f.BtnClose.SetObjectName("BtnClose")
         f.BtnClose.SetSizePolicy(btnSizePolicy)
         f.TitleBarLayout.AddWidget(f.BtnClose, 0, 0)
 
@@ -159,21 +141,116 @@ func (f *QFramelessWindow) setupUI(widget *widgets.QWidget) {
         f.WindowVLayout.AddWidget(f.TitleBar, 0, 0)
         f.WindowVLayout.AddWidget(f.Content, 0, 0)
 
+	// prepare sizegrip
+	f.lefttop = widgets.NewQSizeGrip(widget)
+	f.top = widgets.NewQSizeGrip(widget)
+	f.righttop = widgets.NewQSizeGrip(widget)
+	f.left = widgets.NewQSizeGrip(widget)
+	f.right = widgets.NewQSizeGrip(widget)
+	f.leftbottom = widgets.NewQSizeGrip(widget)
+	f.bottom = widgets.NewQSizeGrip(widget)
+	f.rightbottom = widgets.NewQSizeGrip(widget)
 
-        f.Layout.AddWidget(lefttop, 0, 0, core.Qt__AlignLeft | core.Qt__AlignTop)
-        f.Layout.AddWidget(top, 0, 1, core.Qt__AlignTop)
-        f.Layout.AddWidget(righttop, 0, 2, core.Qt__AlignRight | core.Qt__AlignTop)
-        f.Layout.AddWidget(left, 1, 0, core.Qt__AlignLeft)
+	f.top.SetSizePolicy2(widgets.QSizePolicy__Expanding, widgets.QSizePolicy__Fixed)
+	f.left.SetSizePolicy2(widgets.QSizePolicy__Fixed, widgets.QSizePolicy__Expanding)
+	f.right.SetSizePolicy2(widgets.QSizePolicy__Fixed, widgets.QSizePolicy__Expanding)
+	f.bottom.SetSizePolicy2(widgets.QSizePolicy__Expanding, widgets.QSizePolicy__Fixed)
+
+        f.Layout.AddWidget(f.lefttop, 0, 0, core.Qt__AlignLeft | core.Qt__AlignTop)
+        f.Layout.AddWidget(f.top, 0, 1, core.Qt__AlignTop)
+        f.Layout.AddWidget(f.righttop, 0, 2, core.Qt__AlignRight | core.Qt__AlignTop)
+        f.Layout.AddWidget(f.left, 1, 0, core.Qt__AlignLeft)
         f.Layout.AddWidget(f.WindowWidget, 1, 1, 0)
-        f.Layout.AddWidget(right, 1, 2, core.Qt__AlignRight)
-        f.Layout.AddWidget(leftbottom, 2, 0, core.Qt__AlignLeft | core.Qt__AlignBottom)
-        f.Layout.AddWidget(bottom, 2, 1, core.Qt__AlignBottom)
-        f.Layout.AddWidget(rightbottom, 2, 2, core.Qt__AlignRight | core.Qt__AlignBottom)
+        f.Layout.AddWidget(f.right, 1, 2, core.Qt__AlignRight)
+        f.Layout.AddWidget(f.leftbottom, 2, 0, core.Qt__AlignLeft | core.Qt__AlignBottom)
+        f.Layout.AddWidget(f.bottom, 2, 1, core.Qt__AlignBottom)
+        f.Layout.AddWidget(f.rightbottom, 2, 2, core.Qt__AlignRight | core.Qt__AlignBottom)
 }
 
 func (f *QFramelessWindow) setAttribute() {
 	f.Widget.Window().SetAttribute(core.Qt__WA_TranslucentBackground, true)
 	f.Widget.Window().SetAttribute(core.Qt__WA_NoSystemBackground, true)
+}
+
+func (f *QFramelessWindow) SetStyles(color string) {
+	style := fmt.Sprintf("background-color: %s", color)
+	f.Widget.SetStyleSheet(fmt.Sprintf(" .QFramelessWindow { border: 2px solid #ccc; border-radius: 11px; %s}", style))
+	f.lefttop.SetStyleSheet(fmt.Sprintf("             * { width: 4px; height: 4px; %s}", style))
+	f.top.SetStyleSheet(fmt.Sprintf("                 * { height: 4px; %s}", style))
+	f.righttop.SetStyleSheet(fmt.Sprintf("            * { width: 4px; height: 4px; %s}", style))
+	f.left.SetStyleSheet(fmt.Sprintf("                * { width: 4px;  %s}", style))
+	f.right.SetStyleSheet(fmt.Sprintf("               * { width: 4px;  %s}", style))
+	f.leftbottom.SetStyleSheet(fmt.Sprintf("          * { width: 4px; height: 4px; %s}", style))
+	f.bottom.SetStyleSheet(fmt.Sprintf("              * { height: 4px; %s}", style))
+	f.rightbottom.SetStyleSheet(fmt.Sprintf("         * { width: 4px; height: 4px; %s}", style))
+	f.WindowWidget.SetStyleSheet(fmt.Sprintf(" .QWidget { %s}", style))
+
+	f.BtnMinimize.SetStyleSheet(`
+	#BtnMinimize { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/Minimize.png"); 
+		background-repeat: no-repeat; 
+		background-position: center center; 
+	}
+	#BtnMinimize:hover { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/MinimizeHover.png"); 
+		background-repeat: no-repeat; 
+		background-position: center center;
+	}
+	`)
+	f.BtnMaximize.SetStyleSheet(`
+	#BtnMaximize { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/Maximize.png");
+		background-repeat: no-repeat; 
+		background-position: center center; 
+	}
+	#BtnMaximize:hover { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/MaximizeHover.png"); 
+		background-repeat: no-repeat; 
+		background-position: center center; 
+	}
+	`)
+
+	f.BtnRestore.SetStyleSheet(`
+	#BtnRestore { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/Restore.png");
+		background-repeat: no-repeat; 
+		background-position: center center; 
+	}
+	#BtnRestore:hover { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/RestoreHover.png"); 
+		background-repeat: no-repeat; 
+		background-position: center center; 
+	}
+	`)
+
+	f.BtnClose.SetStyleSheet(`
+	#BtnClose { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/Close.png");
+		background-repeat: no-repeat; 
+		background-position: center center; 
+	}
+	#BtnClose:hover { 
+		background-color:none;
+		border:none;
+		background-image: url(":/icons/CloseHover.png");
+		background-repeat: no-repeat;
+		background-position: center center; 
+	}
+	`)
 }
 
 func (f *QFramelessWindow) setWindowFlags() {
