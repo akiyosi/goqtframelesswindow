b95a96809a8cb9edad50b307e1a76789091ceb66
akiyosi <akiyoshi.maruyama@gmail.com>
2019-03-18 17:35:14 +0900

    trying to impl nativeEvent for windows

--- _example/main.go
+++ _example/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"time"
-
 	frameless "github.com/akiyosi/goqtframelesswindow"
 	"github.com/therecipe/qt/widgets"
 )
@@ -27,32 +25,20 @@ func main() {
 	a.fw.SetTitleColor(200, 200, 200)
 
 	label := widgets.NewQLabel(nil, 0)
-	layout.AddWidget(label, 0, 0)
 	label.SetStyleSheet(" * { color: #eee; }")
-	label.SetText(`
-	General relativity (GR, also known as the general theory of relativity or GTR) is
+	label.SetText(`	General relativity (GR, also known as the general theory of relativity or GTR) is
 	the geometric theory of gravitation published by Albert Einstein in 1915 and 
 	the current description of gravitation in modern physics. `)
 
-	go func() {
-		time.Sleep(16 * 1000 * time.Millisecond)
-		label.SetText("update text 1")
+	layout.AddWidget(label, 0, 0)
 
-		time.Sleep(5 * 1000 * time.Millisecond)
-		label.SetText(`
-		Loop quantum gravity (LQG) is a theory of quantum gravity, 
-		merging quantum mechanics and general relativity, making it
-		a possible candidate for a theory of everything.
-		Its goal is to unify gravity in a common theoretical framework 
-		with the other three fundamental forces of nature, beginning with
-		relativity and adding quantum features. It competes with
-		string theory that begins with quantum field theory and adds gravity.
-		`)
-	}()
 
-	//a.fw.SetNativeEvent(a.app)
+	// // signal arrived during external code execution
+	// a.fw.SetNativeEvent(a.app)
 
 	a.win.Show()
 	a.fw.Widget.SetFocus2()
 	widgets.QApplication_Exec()
+
+	a.fw.SetNativeEvent(a.app)
 }
--- qframelesswindow_windows.go
+++ qframelesswindow_windows.go
@@ -13,21 +13,31 @@ import (
 func (f *QFramelessWindow) SetNativeEvent(app *widgets.QApplication) {
 	filterObj := core.NewQAbstractNativeEventFilter()
 	filterObj.ConnectNativeEventFilter(func(eventType *core.QByteArray, message unsafe.Pointer, result int) bool {
+		fmt.Println(message)
 		msg := (*win.MSG)(message)
 		switch msg.Message {
+		case win.WM_KEYDOWN:
+			return false
 		case win.WM_NCCALCSIZE:
 			fmt.Println("debug")
 			winid := (*win.HWND)(unsafe.Pointer(f.Window.WinId()))
 			style := win.GetWindowLong(*winid, win.GWL_STYLE)
 			style = style | win.WS_MAXIMIZEBOX | win.WS_THICKFRAME | win.WS_CAPTION
 			win.SetWindowLong(*winid, win.GWL_STYLE, style)
-			// case win.WM_NCHITTEST:
-			// 	var winrect *win.RECT
-			//  	winid := (*win.HWND)(unsafe.Pointer(f.Window.WinId()))
-			// 	win.GetWindowRect(*winid, winrect)
-			// 	fmt.Println(winrect.Left, winrect.Bottom)
+			return true
+		case win.WM_NCHITTEST:
+			fmt.Println("debug:: WM_NCHITTEST")
+			var winrect *win.RECT
+		 	winid := (*win.HWND)(unsafe.Pointer(f.Window.WinId()))
+			win.GetWindowRect(*winid, winrect)
+			fmt.Println(winrect.Left, winrect.Bottom)
+			return true
+		default:
+			fmt.Println("debug --", msg.Message)
+			return true
 		}
-		return filterObj.NativeEventFilter(eventType, message, result)
+		
+		// return filterObj.NativeEventFilter(eventType, message, result)
 	})
 	app.InstallNativeEventFilter(filterObj)
 }
