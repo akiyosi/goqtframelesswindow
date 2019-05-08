ba57a4db731acc9627b12d34e0512fc979c92ee7
akiyosi <akiyoshi.maruyama@gmail.com>
2019-05-08 19:49:16 +0900

    Add linux src

---
+++ qframelesswindow_linux.go
@@ -0,0 +1,15 @@
+package qframelesswindow
+
+// import (
+// 	"fmt"
+// )
+
+func (f *QFramelessWindow) SetNativeEvent() {
+	// filterObj := core.NewQAbstractNativeEventFilter()
+	// filterObj.ConnectNativeEventFilter(func(eventType *core.QByteArray, message unsafe.Pointer, result int) bool {
+
+	// 	// return filterObj.NativeEventFilter(eventType, message, result)
+	// 	return false
+	// })
+	// app.InstallNativeEventFilter(filterObj)
+}
