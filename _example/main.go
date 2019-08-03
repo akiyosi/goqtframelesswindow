6c4c0cb6d2bf19a530e219d895161421399e3f85
akiyosi <akiyoshi.maruyama@gmail.com>
2019-08-03 21:06:59 +0900

    remove unnecessary function

--- _example/main.go
+++ _example/main.go
@@ -1,47 +1,47 @@
-// package main
-// 
-// import (
-// 	"time"
-// 
-// 	frameless "github.com/akiyosi/goqtframelesswindow"
-// 	"github.com/therecipe/qt/widgets"
-// )
-// 
-// type framelessTest struct {
-// 	fw *frameless.QFramelessWindow
-// 
-// 	app *widgets.QApplication
-// 	win *widgets.QMainWindow
-// }
-// 
-// func main() {
-// 	a := &framelessTest{}
-// 	a.app = widgets.NewQApplication(0, nil)
-// 
-// 	a.fw = frameless.CreateQFramelessWindow(1.0)
-// 	layout := widgets.NewQVBoxLayout()
-// 	a.fw.SetupContent(layout)
-// 	a.fw.SetupWidgetColor(30, 30, 30)
-// 	a.fw.SetupTitle("frameless test")
-// 	a.fw.SetupTitleColor(200, 200, 200)
-// 
-// 	label := widgets.NewQLabel(nil, 0)
-// 	label.SetStyleSheet(" * { color: #eee; }")
-// 	label.SetText(`	General relativity (GR, also known as the general theory of relativity or GTR) is
-// 	the geometric theory of gravitation published by Albert Einstein in 1915 and 
-// 	the current description of gravitation in modern physics. `)
-// 
-// 	go func() {
-// 		time.Sleep(13000 * time.Millisecond)
-// 		label.SetText(`	Update! `)
-// 		time.Sleep(3000 * time.Millisecond)
-// 		label.SetText(`	Update 2! `)
-// 	}()
-// 
-// 	layout.AddWidget(label, 0, 0)
-// 
-// 	a.fw.Show()
-// 	a.fw.Widget.SetFocus2()
-// 	a.app.Exec()
-// 
-// }
+package main
+
+import (
+	"time"
+
+	frameless "github.com/akiyosi/goqtframelesswindow"
+	"github.com/therecipe/qt/widgets"
+)
+
+type framelessTest struct {
+	fw *frameless.QFramelessWindow
+
+	app *widgets.QApplication
+	win *widgets.QMainWindow
+}
+
+func main() {
+	a := &framelessTest{}
+	a.app = widgets.NewQApplication(0, nil)
+
+	a.fw = frameless.CreateQFramelessWindow(1.0)
+	layout := widgets.NewQVBoxLayout()
+	a.fw.SetupContent(layout)
+	a.fw.SetupWidgetColor(30, 30, 30)
+	a.fw.SetupTitle("frameless test")
+	a.fw.SetupTitleColor(200, 200, 200)
+
+	label := widgets.NewQLabel(nil, 0)
+	label.SetStyleSheet(" * { color: #eee; }")
+	label.SetText(`	General relativity (GR, also known as the general theory of relativity or GTR) is
+	the geometric theory of gravitation published by Albert Einstein in 1915 and 
+	the current description of gravitation in modern physics. `)
+
+	go func() {
+		time.Sleep(13000 * time.Millisecond)
+		label.SetText(`	Update! `)
+		time.Sleep(3000 * time.Millisecond)
+		label.SetText(`	Update 2! `)
+	}()
+
+	layout.AddWidget(label, 0, 0)
+
+	a.fw.Show()
+	a.fw.Widget.SetFocus2()
+	a.app.Exec()
+
+}
--- qframelesswindow.go
+++ qframelesswindow.go
@@ -136,26 +136,6 @@ func (f *QFramelessWindow) RemoveWindowNativeShadow() {
 	f.SetStyleMask()
 }
 
-func (f *QFramelessWindow) SetupWindowShadow(size int) {
-	f.shadowMargin = size
-	f.Layout.SetContentsMargins(f.shadowMargin, f.shadowMargin, f.shadowMargin, f.shadowMargin)
-	if f.shadowMargin == 0 {
-		return
-	}
-	shadow := widgets.NewQGraphicsDropShadowEffect(nil)
-	var alpha int
-	if runtime.GOOS == "darwin" {
-		alpha = 220
-		shadow.SetOffset2(0, 10)
-	} else {
-		alpha = 100
-		shadow.SetOffset2(0, 0)
-	}
-	shadow.SetBlurRadius((float64)(f.shadowMargin))
-	shadow.SetColor(gui.NewQColor3(0, 0, 0, alpha))
-	f.WindowWidget.SetGraphicsEffect(shadow)
-}
-
 func (f *QFramelessWindow) SetupUI(widget *widgets.QWidget) {
 	f.InstallEventFilter(f)
 
