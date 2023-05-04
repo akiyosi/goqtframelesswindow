a0fc55d27d457f44bcda864341bd8ed10f7d551f
akiyosi <akiyoshi.maruyama@gmail.com>
2023-05-05 00:09:15 +0900

    add freebsd file

---
+++ qframelesswindow_freebsd.go
@@ -0,0 +1,17 @@
+package qframelesswindow
+
+// import (
+// 	"fmt"
+// )
+
+func (f *QFramelessWindow) SetNSWindowStyleMask(isVisibleTitlebarButtons bool, R, G, B uint16, alpha float32, isWindowFullscreen bool) {
+}
+
+func (f *QFramelessWindow) SetStyleMask() {
+}
+
+func (f *QFramelessWindow) SetupNativeEvent() {
+}
+
+func (f *QFramelessWindow) SetupNativeEvent2() {
+}
